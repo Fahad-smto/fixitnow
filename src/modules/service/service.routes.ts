d916f5483718ae41de08a190a194dcc2365db94e
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-11 01:07:43 +0600

    create service route created

--- src/modules/service/service.routes.ts
+++ src/modules/service/service.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import * as serviceController from './service.controller';
+import { protect, authorize } from '../../middleware/auth.middleware';
 
 const router = Router();
 
 router.get('/', serviceController.getAllServices);
+router.post('/', protect, authorize('technician'), serviceController.createService);
 
 export default router;
